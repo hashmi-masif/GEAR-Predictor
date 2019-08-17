5acceb668289d5ab045836518785a6b123a9e0f2
udolf15 <hashmi.masif@gmail.com>
2019-08-18 01:19:23 +0530

    added emotion dataset preprocess file

---
+++ preProcessEmotionDataset.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+
+def preprocess_input(x, v2=True):
+    x = x.astype('float32')
+    x = x / 255.0
+    if v2:
+        x = x - 0.5
+        x = x * 2.0
+    return x
+
+def to_categorical(integer_classes, num_classes=2):
+    integer_classes = np.asarray(integer_classes, dtype='int')
+    num_samples = integer_classes.shape[0]
+    categorical = np.zeros((num_samples, num_classes))
+    categorical[np.arange(num_samples), integer_classes] = 1
+    return categorical
+
+def split_data(x, y, validation_split=.2):
+    num_samples = len(x)
+    num_train_samples = int((1 - validation_split)*num_samples)
+    train_x = x[:num_train_samples]
+    train_y = y[:num_train_samples]
+    val_x = x[num_train_samples:]
+    val_y = y[num_train_samples:]
+    train_data = (train_x, train_y)
+    val_data = (val_x, val_y)
+    return train_data, val_data
+
+
+def load_fer2013():
+    data = pd.read_csv('/content/gdrive/My Drive/fer2013.csv')
+    pixels = data['pixels'].tolist()
+    width, height = 48, 48
+    faces = []
+    for pixel_sequence in pixels:
+        face = [int(pixel) for pixel in pixel_sequence.split(' ')]
+        face = np.asarray(face).reshape(width, height)
+        face = cv2.resize(face.astype('uint8'),(64,64) )
+        faces.append(face.astype('float32'))
+    faces = np.asarray(faces)
+    faces = np.expand_dims(faces, -1)
+    emotions = pd.get_dummies(data['emotion']).as_matrix()
+    return faces, emotions
+
+faces,emotions = load_fer2013()
